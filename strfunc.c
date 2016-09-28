c58e1a25eed46e524bbd43c4334f4d903a5e1dfd
Jackson Morgan <jmorgan@stuy.edu>
2016-09-28 11:41:20 -0400

    mystrcpy appears to be functional

--- strfunc.c
+++ strfunc.c
@@ -2,10 +2,36 @@
 #include<stdlib.h>
 #include<string.h>
 
-int length(char s[]){
+int mystrlen(char *s){
   int i = 0;
-  while(s[i]){
+  while(*s){
     i ++;
+    s ++;
   }
   return i;
+  
+}
+
+char * mystrcpy( char *dest, char *source ){
+  while(*source){
+    if(*dest == 0){
+      *dest = 1;
+    }
+    *dest = *source;
+    dest ++;
+    source ++;
+  }
+  dest ++;
+  dest = 0;
+
+}
+
+int main(){
+  printf("length of 'that and this': %d\n", mystrlen("that and this"));
+  char s[100]= "this";
+  char p[100]= "hhh";
+  mystrcpy(p,s);
+  printf("%s\n", p);
+  return 0;
+  
 }

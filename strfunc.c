2cf64d5a2329f44357f15b0cdb9a45b3e3bb08b6
Jackson Morgan <jmorgan@stuy.edu>
2016-09-30 11:34:36 -0400

    strcmp mostly functional

--- strfunc.c
+++ strfunc.c
@@ -39,12 +39,57 @@ char * mystrncat( char *dest, char *source, int n){
   }
   //dest ++;
   dest = 0;
-
-  
-
-
 }
 
+/*int mystrcmp( char *s1, char *s2 ){
+  int sum1 = 0;
+  int sum2= 0;
+  while(*s1){
+    sum1 += *s1;
+    s1 ++;
+  }
+  while(*s2){
+    sum2 += *s2;
+    s2 ++;
+  }
+  printf("s1: %d \n s2: %d\n", sum1,sum2); 
+  return 0;
+}*/
+
+int mystrcmp( char *s1, char *s2 ){
+  int sum1 = 0;
+  int sum2= 0;
+  while(*s1 && *s2){
+    sum1= 0;
+    sum2=0;
+    sum1 += *s1;
+    s1 ++;
+    sum2 += *s2;
+    s2 ++;
+    if(sum1 > sum2){
+      return 1;
+    }
+    if(sum1 < sum2){
+      return -1;
+    }
+  }
+  
+  if((!*s1) && (!*s2)){
+      return 0;
+  }
+
+  if(! (*s1)){
+      return -1;
+    }
+
+  if(! (*s2)){
+      return 1;
+    }
+    
+  return 0;
+}
+ 
+ 
 
 int main(){
   printf("length of 'that and this': %d\n", mystrlen("that and this"));
@@ -56,6 +101,11 @@ int main(){
   mystrncat(p,s, 2);
   printf("%s\n", p);
   printf("%s\n", p);
+  char a[100]= "this";
+  char b[100]= "bhat";
+  printf("compare real: %d\n", strcmp(b,a));
+  printf("compare mine: %d\n", mystrcmp(b,a));
+  // issue when different sized
   return 0;
   
 }
